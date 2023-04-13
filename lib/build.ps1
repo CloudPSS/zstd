b34b3804c6615a3d88e492f2569e6509c9594a83
OpportunityLiu <opportunity@live.in>
2023-04-13 20:57:10 +0800

    v0.0.12

--- lib/build.ps1
+++ lib/build.ps1
@@ -6,19 +6,3 @@ bash ./create_single_file_library.sh
 Copy-Item ./zstd.c ../../../zstd_lib.c
 
 Pop-Location
-
-Push-Location "$PSScriptRoot"
-
-docker run --rm -v ${PWD}:/src emscripten/emsdk `
-  emcc ./zstd_lib.c -o ./zstd.js `
-  -flto --closure 1 -O3 `
-  --memory-init-file 0 `
-  -s EXPORTED_FUNCTIONS="['_ZSTD_isError', '_ZSTD_getFrameContentSize', '_ZSTD_decompress', '_ZSTD_compress', '_ZSTD_compressBound', '_malloc', '_free']" `
-  -s FILESYSTEM=0 `
-  -s ALLOW_MEMORY_GROWTH=1 `
-  -s SINGLE_FILE=1 `
-  -s INCOMING_MODULE_JS_API="[]" `
-  -s ENVIRONMENT="web,worker" `
-  -s EXPORT_ES6
-
-Pop-Location
