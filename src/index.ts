b02c1823d1b4767124ac4aad6e57d7b42b414498
OpportunityLiu <opportunity@live.in>
2023-04-14 20:49:05 +0800

    test: node 14

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,22 @@
-import type * as Napi from './napi.js';
-import type * as Wasm from './wasm.js';
+let lib: Omit<typeof import('./napi.js'), 'TYPE'> &
+    Pick<typeof import('./napi.js') | typeof import('./wasm.js'), 'TYPE'>;
 
-const lib: Omit<typeof Napi, 'TYPE'> & Pick<typeof Wasm | typeof Napi, 'TYPE'> = await (async () => {
-    try {
-        return await import('./napi.js');
-    } catch {
-        const { compress, decompress, TYPE } = await import('./wasm.js');
-        return {
-            compress: (data, level) => {
-                const result = compress(data, level);
-                return Buffer.from(result.buffer, result.byteOffset, result.byteLength);
-            },
-            decompress: (data) => {
-                const result = decompress(data);
-                return Buffer.from(result.buffer, result.byteOffset, result.byteLength);
-            },
-            TYPE,
-        };
-    }
-})();
+try {
+    lib = await import('./napi.js');
+} catch {
+    const { compress, decompress, TYPE } = await import('./wasm.js');
+    lib = {
+        compress: (data, level) => {
+            const result = compress(data, level);
+            return Buffer.from(result.buffer, result.byteOffset, result.byteLength);
+        },
+        decompress: (data) => {
+            const result = decompress(data);
+            return Buffer.from(result.buffer, result.byteOffset, result.byteLength);
+        },
+        TYPE,
+    };
+}
 
 /** ZStandard compress */
 export const compress = lib.compress;
@@ -26,4 +24,4 @@ export const compress = lib.compress;
 /** ZStandard decompress */
 export const decompress = lib.decompress;
 
-export const TYPE: typeof import('./napi.js').TYPE | typeof import('./wasm.js').TYPE = lib.TYPE;
+export const TYPE: typeof lib.TYPE = lib.TYPE;
