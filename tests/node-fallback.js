8456b15e00be8893054535de57d419255f1c36ca
OpportunityLiu <opportunity@live.in>
2023-04-14 21:09:44 +0800

    ci: drop lts/-2

--- tests/node-fallback.js
+++ tests/node-fallback.js
@@ -6,20 +6,14 @@ jest.mock('node-gyp-build', () => {
     });
 });
 
-const delay = (/** @type {number | undefined} */ timeout) => new Promise((resolve) => setTimeout(resolve, timeout));
-
 it('should fallback to wasm', async () => {
     const root = await import('@cloudpss/zstd');
-    // Test will fail in node 14 without this
-    await delay(1000);
     expect(jest.requireMock('node-gyp-build')).toBeCalledWith(expect.any(String));
     expect(root.TYPE).toBe('wasm');
 });
 
 it('should returns buffer', async () => {
     const root = await import('@cloudpss/zstd');
-    // Test will fail in node 14 without this
-    await delay(1000);
     expect(root.TYPE).toBe('wasm');
     expect(root.compress(Buffer.alloc(10))).toBeInstanceOf(Buffer);
     expect(root.decompress(root.compress(Buffer.alloc(10)))).toBeInstanceOf(Buffer);
