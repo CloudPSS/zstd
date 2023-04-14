ddde59599c3ff563f0feaf78d449e5974e04b444
OpportunityLiu <opportunity@live.in>
2023-04-14 19:50:58 +0800

    test: fix node 14

--- tests/node-fallback.js
+++ tests/node-fallback.js
@@ -8,12 +8,16 @@ jest.mock('node-gyp-build', () => {
 
 it('should fallback to wasm', async () => {
     const root = await import('@cloudpss/zstd');
+    // Test will fail in node 14 without this
+    await Promise.resolve();
     expect(jest.requireMock('node-gyp-build')).toBeCalledWith(expect.any(String));
     expect(root.TYPE).toBe('wasm');
 });
 
 it('should returns buffer', async () => {
     const root = await import('@cloudpss/zstd');
+    // Test will fail in node 14 without this
+    await Promise.resolve();
     expect(root.TYPE).toBe('wasm');
     expect(root.compress(Buffer.alloc(10))).toBeInstanceOf(Buffer);
     expect(root.decompress(root.compress(Buffer.alloc(10)))).toBeInstanceOf(Buffer);
