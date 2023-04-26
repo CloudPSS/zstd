80d1444ef0e80d957914a5d459ea5dda74a064cf
OpportunityLiu <opportunity@live.in>
2023-04-26 10:16:34 +0800

    fix: remove assert, add error name

--- lib/build-wasm.ps1
+++ lib/build-wasm.ps1
@@ -8,13 +8,14 @@ docker run --rm -v ${PWD}:/src emscripten/emsdk `
   --memory-init-file 0 `
   -sSTRICT `
   -sMALLOC=emmalloc `
-  -sEXPORTED_FUNCTIONS="['_ZSTD_versionNumber', '_ZSTD_isError', '_ZSTD_getFrameContentSize', '_ZSTD_decompress', '_ZSTD_compress', '_ZSTD_compressBound', '_malloc', '_free', '_usedmem']" `
+  -sEXPORTED_FUNCTIONS="['_ZSTD_versionNumber', '_ZSTD_isError', '_ZSTD_getErrorName', '_ZSTD_getFrameContentSize', '_ZSTD_decompress', '_ZSTD_compress', '_ZSTD_compressBound', '_malloc', '_free', '_usedmem']" `
   -sFILESYSTEM=0 `
   -sALLOW_MEMORY_GROWTH=1 `
   -sSINGLE_FILE=1 `
   -sINCOMING_MODULE_JS_API="[]" `
+  -sEXTRA_EXPORTED_RUNTIME_METHODS="['UTF8ToString']" `
   -sMODULARIZE `
   -sEXPORT_ES6 `
-$(if ($DEBUG) { "-O1", "-g3", "-sASSERTIONS=1" } else { "-flto", "-O3", "--closure", "1" } )
+$(if ($DEBUG) { "-O1", "-g3", "-sASSERTIONS=1", "-DDEBUG" } else { "-flto", "-O3", "--closure", "1", "-DNDEBUG" } )
 
 Pop-Location
--- prebuilds/zstd.d.ts
+++ prebuilds/zstd.d.ts
@@ -3,9 +3,16 @@ type Ptr = number & { ptr: 'void*' };
 type Module = {
     readonly HEAP8: Int8Array;
     readonly HEAPU8: Uint8Array;
+    readonly HEAP16: Int16Array;
+    readonly HEAPU16: Uint16Array;
+    readonly HEAP32: Int32Array;
+    readonly HEAPU32: Uint32Array;
+    readonly HEAPF32: Float32Array;
+    readonly HEAPF64: Float64Array;
 
     _ZSTD_versionNumber(): number;
     _ZSTD_isError(code: number): number;
+    _ZSTD_getErrorName(code: number): Ptr;
     _ZSTD_compressBound(size: number): number;
     _ZSTD_compress(outPtr: Ptr, outSize: number, inPtr: Ptr, inSize: number, level: number): number;
     _ZSTD_getFrameContentSize(src: number, size: number): number;
@@ -13,5 +20,7 @@ type Module = {
     _malloc(size: number): Ptr;
     _free(ptr: Ptr): void;
     _usedmem(): number;
+
+    UTF8ToString(ptr: Ptr, maxBytesToRead?: number): string;
 };
 export default function createModule(): Promise<Module>;
