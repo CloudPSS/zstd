1a1b62e2b50b2505940e1782df450d1766477281
OpportunityLiu <opportunity@live.in>
2024-05-07 15:16:20 +0800

    feat(napi): web stream

--- tests/stream.ts
+++ tests/stream.ts
@@ -3,6 +3,7 @@ import { Readable } from 'node:stream';
 import { buffer } from 'node:stream/consumers';
 import { TransformStream } from 'node:stream/web';
 import * as wasm from '@cloudpss/zstd/wasm';
+import * as napi from '@cloudpss/zstd/napi';
 
 const randomBuffer = randomBytes(1000);
 /**
@@ -29,5 +30,41 @@ describe('wasm stream compress api', () => {
         );
         expect(data).toBeInstanceOf(Uint8Array);
         expect(asBuffer(data)).toEqual(randomBuffer);
+    });
+});
+
+describe('napi stream compress api', () => {
+    it('should compress', async () => {
+        const readable = Readable.toWeb(Readable.from([randomBuffer]));
+        const data = await buffer(
+            Readable.fromWeb(readable.pipeThrough(napi.compressor(3) as TransformStream<BinaryData, Uint8Array>)),
+        );
+        expect(data).toBeInstanceOf(Buffer);
+        expect(napi.decompress(data)).toEqual(randomBuffer);
+    });
+
+    it('should decompress', async () => {
+        const readable = Readable.toWeb(Readable.from([napi.compress(randomBuffer)]));
+        const data = await buffer(
+            Readable.fromWeb(readable.pipeThrough(napi.decompressor() as TransformStream<BinaryData, Uint8Array>)),
+        );
+        expect(data).toBeInstanceOf(Buffer);
+        expect(data).toEqual(randomBuffer);
+    });
+});
+
+describe('napi node stream compress api', () => {
+    it('should compress', async () => {
+        const readable = Readable.from([randomBuffer]);
+        const data = await buffer(readable.pipe(new napi.Compressor(3)));
+        expect(data).toBeInstanceOf(Buffer);
+        expect(napi.decompress(data)).toEqual(randomBuffer);
+    });
+
+    it('should decompress', async () => {
+        const readable = Readable.from([napi.compress(randomBuffer)]);
+        const data = await buffer(readable.pipe(new napi.Decompressor()));
+        expect(data).toBeInstanceOf(Buffer);
+        expect(data).toEqual(randomBuffer);
     });
 });
