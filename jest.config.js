5558734cb3f95db3b028dbc224ecadc1b856bcac
OpportunityLiu <opportunity@live.in>
2024-08-12 11:42:54 +0800

    test: try fix error

--- jest.config.js
+++ jest.config.js
@@ -123,9 +123,7 @@ export default /** @type {import('ts-jest').JestConfigWithTsJest} */ ({
     // rootDir: undefined,
 
     // A list of paths to directories that Jest should use to search for files in
-    // roots: [
-    //   "<rootDir>"
-    // ],
+    roots: ['<rootDir>'],
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
@@ -174,6 +172,7 @@ export default /** @type {import('ts-jest').JestConfigWithTsJest} */ ({
             'ts-jest',
             {
                 useESM: true,
+
                 tsconfig: '<rootDir>/tests/tsconfig.json',
             },
         ],
